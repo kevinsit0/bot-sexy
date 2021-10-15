2e35f5293997561ee54988409451508204b68a46
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-15 17:17:00 -0500

    Add files via upload

--- plugins/bot.js
+++ plugins/bot.js
@@ -1,8 +1,8 @@
-let handler = m => m.reply('*Si... si estoy aqui.*👺 , *¿En que te puedo ayudar? Escribe_ *.?* o *.menu* _para que puedas ver mis funciones._*')
+let handler = async (m, { conn }) => {
+    conn.sendFile(m.chat, 'media/sticker/Bot.webp', 'media/sticker/Bot1.webp', 'media/sticker/Bot2.webp', m)
+}
 
-handler.customPrefix = /^(bot)$/i
+handler.customPrefix = /^bot$/i
 handler.command = new RegExp
-handler.private = false
-handler.group = true
 
 module.exports = handler
