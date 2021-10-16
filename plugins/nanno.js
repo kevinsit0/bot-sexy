33fb3f21f422d305d3906c0c27857ff573282dc4
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-16 16:24:47 -0500

    Update nanno.js

--- plugins/nanno.js
+++ plugins/nanno.js
@@ -2,14 +2,14 @@ let { Presence } = require('@adiwajshing/baileys')
 let fetch = require('node-fetch')
 
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args || !args[0]) return conn.sendFile(m.chat, '', '', '', m)
+  if (!args || !args[0]) return conn.sendFile(m.chat, 'media/nannoRisa(2).mp3', '', '', m)
 	let text = args.join` `
 	fetch("https://api.simsimi.net/v2/?text=" + encodeURIComponent(text)+ "&lc=es")
   .then(res => res.json())
   .then(batch => {
     conn.updatePresence(m.chat, Presence.composing)
   conn.reply(m.chat, `${batch.success}`, m)
-  }) .catch(() => { conn.reply(m.chat, `_¡La función jalea está desabilitada!_`, m) })
+  }) .catch(() => { conn.reply(m.chat, `_¡La función Nanno está desabilitada!_`, m) })
 }
 handler.help = ['nannonanno'].map(v => v + ' *texto*')
 handler.tags = ['fun']
