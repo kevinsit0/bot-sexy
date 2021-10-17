f1b0f3c7273697f1eceda2062f996ac66dbbadb2
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-17 17:03:01 -0500

    Update Jair.js

--- plugins/Jair.js
+++ plugins/Jair.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn, text }) => {
   let chats = conn.chats.all().filter(v => !v.read_only && v.message && !v.archive).map(v => v.jid)
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
-  let content = conn.cMod(m.chat, cc, /tc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 hola soy nanno mucho gusto」')
+  let content = conn.cMod(m.chat, cc, /tc|broadcast/i.test(text) ? text :  + readMore + '「 hola soy nanno mucho gusto」')
   for (let id of chats) conn.copyNForward(id, content, true)
 }
 handler.customPrefix = /^(jair)$/i
