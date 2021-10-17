c31d703967e79a90e505f87b1b678edf7e41834f
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-17 17:23:33 -0500

    Delete Lala.js

--- plugins/Lala.js
+++
@@ -1,8 +0,0 @@
-let handler = m => m.reply('Hola👋 soy bot sexy ' , '0@s.whatsapp.net')
-
-handler.customPrefix = /^(lala)$/i
-handler.command = new RegExp
-handler.private = false
-handler.group = true
-
-module.exports = handler
