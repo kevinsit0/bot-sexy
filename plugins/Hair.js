ce24a55e7143a7ef012fcc94feaf6b19e171c966
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-17 17:20:49 -0500

    Delete Hair.js

--- plugins/Hair.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix: _p }) => {
-  conn.fakeReply(m.chat, '*Hola👋 soy bot sexy 🌚??* , *En que te puedo ayudar¿?*_Escribe_ *#help* o *#Menú* _para que puedas ver mis funciones_ .*', '0@s.whatsapp.net', '👿𝐛𝐨𝐭-𝐧𝐚𝐧𝐧𝐨😈')
-
-handler.customPrefix = /^(jair)$/i
-handler.command = new RegExp
-handler.private = false
-handler.group = true
-
-module.exports = handler
