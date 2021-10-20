3966c55fcb1b20b8e576cb2e28438606b1689cea
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-20 11:47:30 -0500

    Update photooxy.js

--- plugins/photooxy.js
+++ plugins/photooxy.js
@@ -8,8 +8,8 @@ let handler = async (m, { conn, usedPrefix, args: [effect], text: txt }) => {
   text2 = text2.join(split)
   let url = global.API('xteam', '/photooxy/' + effect, { text, text2 }, 'APIKEY')
   if(!text) throw `Formato Incorrecto\n\nEjemplo:\n*${usedPrefix}wolfmetal sexy*\n*${usedPrefix}battlefield sexy|bot*`
-  conn.fakeReply(m.chat, '*ESPERA ⏳*', '0@s.whatsapp.net', '✨𝐛𝐨𝐭-𝒔𝒆𝒙𝒚✨')
-  await conn.sendFile(m.chat, url, 'photooxy.jpg', `✨𝐛𝐨𝐭-𝒔𝒆𝒙𝒚✨\n\n*Efecto:* ${effect}`, m)
+  conn.fakeReply(m.chat, '*ESPERA ⏳*', '0@s.whatsapp.net', '👿𝐛𝐨𝐭-𝐧𝐚𝐧𝐧𝐨😈')
+  await conn.sendFile(m.chat, url, 'photooxy.jpg', `👿𝐛𝐨𝐭-𝐧𝐚𝐧𝐧𝐨😈\n\n*Efecto:* ${effect}`, m)
   .catch(() => { conn.reply(m.chat, `_Error! Formato incorrecto o inténtalo más tarde_`, m) })
 }
 handler.help = ['photooxy'].map(v => v + ' *(efecto)* *texto|texto2*')
