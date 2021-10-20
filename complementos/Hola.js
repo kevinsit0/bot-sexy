d2684676308170a4e7b183629b264a20160ae47b
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-20 11:53:58 -0500

    Delete Hola.js

--- complementos/Hola.js
+++
@@ -1,8 +0,0 @@
-let handler = m => m.reply('*Hola👋 soy 👿𝐛𝐨𝐭-𝐧𝐚𝐧𝐧𝐨😈* , *En que te puedo ayudar¿?*_Escribe_ *#help* o *#Menú* _para que puedas ver mis funciones_ .*')
-
-handler.customPrefix = /^(Hola)$/i 
-handler.command = new RegExp
-handler.private = false
-handler.group = true
-
-module.exports = handler
