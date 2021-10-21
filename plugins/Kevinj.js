fa26720de0da76b77aeb54236a7774ff8b07762d
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-21 16:57:40 -0500

    Delete Kevinj.js

--- plugins/Kevinj.js
+++
@@ -1,9 +0,0 @@
-let handler = async(m, { conn, usedPrefix: _p }) => {
-conn.fakeReply(m.chat, '*kevinsisksksk⏳*', '0@s.whatsapp.net', '👿𝐛𝐨𝐭-𝐧𝐚𝐧𝐧𝐨😈')
-
-handler.customPrefix = /^(kevin)$/i
-handler.command = new RegExp
-handler.private = false
-handler.group = true
-
-module.exports = handler
