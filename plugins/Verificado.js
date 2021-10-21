4d08bc29dcdb400bd78ba06f8bb256163ed13066
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-21 17:36:49 -0500

    Update Verificado.js

--- plugins/Verificado.js
+++ plugins/Verificado.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     conn.sendFile(m.chat,'media/sticker/Bot1.webp', '', 'xd', m)
 }
 
-handler.customPrefix = /^jajaja?$/i
+handler.customPrefix = /jajaja|jsjsj|JAJAJAJA/
 handler.command = new RegExp
 
 module.exports = handler
