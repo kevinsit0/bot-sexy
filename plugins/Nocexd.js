6cff99170c54f429d5541020112f9dc427cec869
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-21 17:59:05 -0500

    Delete Nocexd.js

--- plugins/Nocexd.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-  let old = performance.now()
-  await conn.sendFile(m.chat, '_Test de velocidad..._', '0@s.whatsapp.net', '👿𝐛𝐨𝐭-𝐧𝐚𝐧𝐧𝐨😈')
-  let neww = performance.now()
-  }
-  handler.customPrefix = /^kevin?$/i
-handler.command = new RegExp
-
-module.exports = handler
