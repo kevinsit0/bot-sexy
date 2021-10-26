afb5fee8674cb53560e38a553c121ca01a6e6f2e
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-26 13:19:53 -0500

    Delete Nick.js

--- plugins/Nick.js
+++
@@ -1,8 +0,0 @@
-let handler = m => m.reply('*Hola👋que tal soy 👿𝐛𝐨𝐭-𝐧𝐚𝐧𝐧𝐨😈 ten las iniciales* ,    *ABDㅤNICK*')
-
-handler.customPrefix = /^(nick)$/i
-handler.command = new RegExp
-handler.private = false
-handler.group = true
-
-module.exports = handler
