e2b11d391f1ab76b044f7512909d5f4459e11f90
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-26 13:03:21 -0500

    Update nick5.js

--- plugins/nick5.js
+++ plugins/nick5.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn,usedPrefix }) => {
-    conn.fakeReply(m.chat, `espera estoy buscando las iniciales
+    conn.fakeReply(m.chat, `espera estoy buscando las iniciales`,
 '0@s.whatsapp.net',
 '👿𝐛𝐨𝐭-𝐧𝐚𝐧𝐧𝐨😈'
 .trim()) 
