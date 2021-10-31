3166e6710106a14ef486528f45380b526e297c19
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-10-31 13:55:30 -0500

    Delete presentacion.js

--- plugins/presentacion.js
+++
@@ -1,8 +0,0 @@
-let handler = m => m.reply('*Hola👋 soy 👿𝐛𝐨𝐭-𝐧𝐚𝐧𝐧𝐨😈 asistente personal de kevin* , *Un Bot de WhatsApp es un programa de chatbot o software que se puede utilizar específicamente en la popular aplicación de mensajería encriptada WhatsApp. Un Bot de WhatsApp te introduce en el mundo del marketing conversacional y te ayuda a administrar los problemas de tus clientes.*')
-
-handler.customPrefix = /^(presentar)$/i
-handler.command = new RegExp
-handler.private = false
-handler.group = true
-
-module.exports = handler
