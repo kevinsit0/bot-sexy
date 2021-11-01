44dcd32dfe6c6997f04f03f3fa8c00b61e41c261
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-11-01 12:52:15 -0500

    Delete hola😈.js

--- plugins/hola😈.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendFile(m.chat, 'media/sticker/Sticker😈.webp', '', 'xd', m)
-}
-
-handler.customPrefix = /^hola$/i
-handler.command = new RegExp
-
-module.exports = handler
