f0e36badf20bdb4ee30ad0b8d19fd8eb3c13b63d
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-11-03 20:37:23 -0500

    Update sbro.js

--- plugins/sbro.js
+++ plugins/sbro.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     conn.sendFile(m.chat, 'media/sticker/Bro.webp', '', 'xd', m)
 }
 
-handler.customPrefix = /bro|Bro?/i
+handler.customPrefix = /^bro$/i
 handler.command = new RegExp
 
 module.exports = handler
