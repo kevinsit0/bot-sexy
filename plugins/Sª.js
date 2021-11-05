d5006de73b812556f2f4e3847dd3fa92c048b3a2
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-11-05 11:01:11 -0500

    Update Sª.js

--- plugins/Sª.js
+++ plugins/Sª.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     conn.sendFile(m.chat, 'media/sticker/Stikerª.webp', '', 'xd', m)
 }
 
-handler.customPrefix = /^a$/i
+handler.customPrefix = /^a/i
 handler.command = new RegExp
 
 module.exports = handler
