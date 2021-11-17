852e6819dd80390e99efffa6193a54501c71361e
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-11-17 09:25:37 -0500

    Add files via upload

---
+++ plugins/Unbanchat1.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn }) => {
+  global.db.data.chats[m.chat].isBanned = false
+  m.reply('Bot de regreso al grupo!')
+}
+handler.help = ['unbanchat1']
+handler.tags = ['owner']
+handler.command = /^unbanchat$/i
+handler.owner,handler.mods = true
+
+module.exports = handler
