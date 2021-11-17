f64cb39f8ccefb394e4e7ac9ec24d2565ca7a428
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-11-17 09:40:43 -0500

    Update Unbanchat1.js

--- plugins/Unbanchat1.js
+++ plugins/Unbanchat1.js
@@ -1,7 +1,10 @@
 let handler = async (m, { conn }) => {
+    // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
   global.db.data.chats[m.chat].isBanned = false
   m.reply('Bot de regreso al grupo!')
+    // } else m.reply('Ada nomor host disini...')
 }
+
 handler.help = ['unbanchat1']
 handler.tags = ['owner']
 handler.command = /^unbanchat1$/i
