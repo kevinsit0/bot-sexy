ea1df772abf4aaf14fe77f57e0f7d7c22853e134
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2021-11-21 09:34:03 -0500

    Update sno.js

--- plugins/sno.js
+++ plugins/sno.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     conn.sendFile(m.chat, 'media/sticker/SNo.webp', '', 'xd', m)
 }
 
-handler.customPrefix = /^si|siu?$/i
+handler.customPrefix = /^si|siu|simon|si bro$/i
 handler.command = new RegExp
 
 module.exports = handler
