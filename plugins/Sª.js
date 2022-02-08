c43445cd7ece21848cc87e0d03730d79796fa10a
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-02-08 11:00:12 -0500

    Delete Sª.js

--- plugins/Sª.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendFile(m.chat, 'media/sticker/Stikerª.webp', '', 'xd', m)
-}
-
-handler.customPrefix = /^a$/i
-handler.command = new RegExp
-
-module.exports = handler
