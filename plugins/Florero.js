179115b9d37a149ca6cd49263c896415ff4ec720
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-02-08 10:20:41 -0500

    Delete Florero.js

--- plugins/Florero.js
+++
@@ -1,9 +0,0 @@
-let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/Florero.jpg', '', 'tengo este florero', m)
-}
-handler.customPrefix = /^(florero)$/i
-handler.command = new RegExp
-handler.private = true
-
-
-module.exports = handler
