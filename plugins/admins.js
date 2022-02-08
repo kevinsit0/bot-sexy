e9efbeb554082f3d5e2378bf60fd18b4317ed808
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-02-08 09:56:30 -0500

    Update admins.js

--- plugins/admins.js
+++ plugins/admins.js
@@ -31,7 +31,7 @@ ${listAdmin}
         conn.sendFile(m.key.remoteJid, pp, 'pp.jpg', text, m, false, { contextInfo: { mentionedJid } })
     }
 }
-handler.command = /^(@admins)$/i
+handler.command = /^(admins|@admins)$/i
 
 handler.group = true
 
