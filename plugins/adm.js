7b647b50760bdb151b2ccd49b7d3bdb94c36a5e5
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-02-08 09:52:12 -0500

    Delete adm.js

--- plugins/adm.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'huh'
-  if (isAdmin) throw 'Ya soy administrador 😊'
-  await conn.groupMakeAdmin(m.chat, [m.sender])
-}
-handler.command = /^adm$/i
-handler.rowner = true
-handler.botAdmin = true
-module.exports = handler
