5f03b9c47022e4acfbad0d422dfcd8aa510173d8
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-02-13 14:34:33 -0500

    Delete Stikerjajsja.js

--- plugins/Stikerjajsja.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendFile(m.chat,'media/sticker/Bot1.webp', '', 'xd', m)
-}
-
-handler.customPrefix = /jajaja|jsjsj|JAJAJAJA/i
-handler.command = new RegExp
-
-module.exports = handler
