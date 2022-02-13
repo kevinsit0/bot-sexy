d2770dbce4a6a60e1cbc7c8f1885f14841749694
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-02-13 14:37:05 -0500

    Delete sxd.js

--- plugins/sxd.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendFile(m.chat, 'media/sticker/Xd😂.webp', '', 'xd', m)
-}
-
-handler.customPrefix = /^xd$/i
-handler.command = new RegExp
-
-module.exports = handler
