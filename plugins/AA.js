448a47d6bdac84b363f3a1e04b5db9f70a47b1fe
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-02-14 10:34:14 -0500

    Add files via upload

---
+++ plugins/AA.js
@@ -0,0 +1,19 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn }) => {
+	let url = anime[Math.floor(Math.random() * anime.length)]
+	await conn.sendMessage(m.chat, {
+		contentText: '*TE VA A CAER LA POLICÍA 🚔️*',
+		footerText: '©The Shadow Brokers - Bot',
+		buttons: [
+			{ buttonId: '.bienvenido', buttonText: { displayText: 'SIGUIENTE' }, type: 1 }
+		],
+		headerType: 4,
+		imageMessage: (await conn.prepareMessageMedia(await (await fetch(url)).buffer(), 'imageMessage', {})).imageMessage
+	}, 'buttonsMessage', { quoted: m })
+}
+handler.command = /^(bienvenido)$/i
+handler.tags = ['internet']
+handler.help = ['bienvenido']
+module.exports = handler
+
