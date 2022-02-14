c4495d567b83ac03774deb057d825e8e49eef262
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-02-14 10:21:49 -0500

    Add files via upload

--- plugins/vps.js
+++ plugins/vps.js
@@ -1,10 +1,27 @@
-let handler = async (m, { conn,usedPrefix }) => {
-    conn.fakeReply(m.chat, `media/sticker/1.webp`,
-'0@s.whatsapp.net',
-'👿𝐛𝐨𝐭-𝐧𝐚𝐧𝐧𝐨😈'
-.trim()) 
+let moment = require('moment-timezone')
+let handler = async (m, { conn}) => {
+    let {registered } = global.db.data.users[m.sender]
+    let name = registered ? global.db.data.users[m.sender].name : conn.getName(m.sender)
+    conn.sendButton(m.chat, `${ucapan()}, *${name}*`, `_Presione el botón "VPS" para ver las opciones_\n\nᴡᴀʟʟ•🅔 ᴮᴼᵀ`, 'VPS', `.vps`)
 }
-handler.command = /^vps$/i
+
+handler.customPrefix = /^(serv|serve|server|servidores|server|vps|servi|servidor|serví)$/i
+handler.command = new RegExp
 handler.group = true
+handler.disabled= false
 
 module.exports = handler
+
+function ucapan() {
+    const time = moment.tz('America/Guayaquil').format('HH')
+    if (time >= 00) {
+      res = "Buenos días"
+    }
+    if (time >= 12) {
+      res = "Buenas tardes"
+    }
+    if (time >= 18) {
+      res = "Buenas noches"
+    }
+    return res
+  }
