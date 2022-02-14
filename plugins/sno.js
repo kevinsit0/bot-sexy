c780303ec11ddef67047acf636ef516c93b5a77e
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-02-14 09:35:47 -0500

    Delete sno.js

--- plugins/sno.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendFile(m.chat, 'media/sticker/SNo.webp', '', 'xd', m)
-}
-
-handler.customPrefix = /^si$|siu$|simon$|si bro$/i
-handler.command = new RegExp
-
-module.exports = handler
