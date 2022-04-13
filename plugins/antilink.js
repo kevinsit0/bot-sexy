860dc9eab91a7352c437ba84ec7385d4ea5f95b0
kevinsit0 <84455918+kevinsit0@users.noreply.github.com>
2022-04-12 19:18:54 -0500

    Update antilink.js

--- plugins/antilink.js
+++ plugins/antilink.js
@@ -1,22 +1,26 @@
-let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-handler.before = function (m, { user, bot, groupMetadata }) {
-  if (m.isBaileys && m.fromMe) return true
+let handler = m => m
+
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
+  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    m.reply('*Adiós👋, rompiste las reglas serás eliminado...!!*')
-if (user.isAdmin || user.isSuperAdmin) return m.reply('*Uh te salvaste eres admin, no serás eliminado*')
-    let participants = m.isGroup ? groupMetadata.participants : []
-    let bot = m.isGroup ? participants.find(u => u.jid == this.user.jid) : {}
-    if (bot.isAdmin || bot.isSuperAdmin) {
-        let linkGC = this.groupInviteCode(m.chat)
-        let isLinkThisGc = new RegExp(linkGC, 'g')
-        let isgclink = isLinkThisGc.exec(m.text)
-        if (isgclink)
-        conn.groupRemove(m.chat, [m.sender])
-        
-    } else m.reply('*El bot no es admin, no se puede exterminar a las personas*')
+    await m.reply(`*「 ANTI LINKS 」*\n*Hasta la vista baby👋, ${await this.getName(m.sender)} rompiste las reglas serás exterminado....!!*`)
+    await m.reply(`*Tienes 3 segundos para eliminar el link y retractarte...!!!!*`)
+    await m.reply(`*3!!*`)
+    await m.reply(`*2!!*`)
+    await m.reply(`*1!!*`)
+    if (isAdmin) return m.reply('*Te salvaste cagon(a) eres admin, no puedo eliminarte :v*')
+    if (!isBotAdmin) return m.reply('*El bot no es admin, no puede exterminar a las personas*')
+    let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
+    let isLinkThisGc = new RegExp(linkGC, 'i')
+    let isgclink = isLinkThisGc.test(m.text)
+    if (isgclink) return m.reply('*Lol.. enviaste el enlace de este grupo :v*')
+    await this.groupRemove(m.chat, [m.sender])
   }
   return true
 }
+
+module.exports = handler
